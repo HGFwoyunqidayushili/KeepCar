a01de804fb80fe56b4c528e08d85f1923bed8f2c
liurongb <1270969067@qq.com>
2017-12-28 16:51:03 +0800

    刘荣斌4s布局重试

---
+++ app/src/main/java/jiyun/com/keepcar/ui/homepage/forecar/Car_details.java
@@ -0,0 +1,16 @@
+package jiyun.com.keepcar.ui.homepage.forecar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import jiyun.com.keepcar.R;
+
+public class Car_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_details);
+    }
+}
